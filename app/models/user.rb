478068cff95d341e81f6717f7646dce9d514666a
Vladimír Mrkáček <vlad.mrkacek@gmail.com>
2019-11-26 15:28:48 +0100

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
+  validates :username, presence: true
 
   auto_strip_attributes :email, squish: true
 
